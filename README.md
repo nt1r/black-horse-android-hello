fdfc1bb51849c51d2a769a09f7062501aa04ed58
Leqi <michaelleqihust@gmail.com>
2020-10-23 14:28:24 +0800

    [leqi] feat: 完成FrameLayout和ScrollView作业。

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 ![linear-layout-en](./screenshots/linear-layout-en.jpeg)
 
 ![linear-layout-zh](./screenshots/linear-layout-zh.jpeg)
+
+## FrameLayout and ScrollView
+
+![linear-layout-zh](./screenshots/frame-layout-en.jpeg)
+
+![linear-layout-zh](./screenshots/frame-layout-zh.jpeg)
--- app/src/main/java/com/example/black_horse_onboarding/MainActivity.kt
+++ app/src/main/java/com/example/black_horse_onboarding/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.black_horse_onboarding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var linearLayout: LinearLayout
+    private val BUTTON_COUNT: Int = 10
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        linearLayout = findViewById(R.id.linear_layout)
+
+        for (index in 1..BUTTON_COUNT + 1) {
+            val view: View = layoutInflater.inflate(R.layout.button_linear_layout, null)
+            val button: MaterialButton = view as MaterialButton
+            button.text = String.format(getString(R.string.linear_layout_button), index)
+            linearLayout.addView(button)
+        }
     }
 }
