5b392a677c4ff0a83699d5bb4c9b5205c60e489b
Leqi <michaelleqihust@gmail.com>
2020-10-26 16:54:36 +0800

    [leqi] feat: 完成Activity生命周期作业。

--- app/src/main/java/com/example/black_horse_onboarding/ContactsDetailActivity.kt
+++ app/src/main/java/com/example/black_horse_onboarding/ContactsDetailActivity.kt
@@ -1,11 +1,15 @@
 package com.example.black_horse_onboarding
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatImageView
 import com.example.black_horse_onboarding.view_model.ContactVM
 import com.google.gson.Gson
@@ -18,10 +22,15 @@ class ContactsDetailActivity : AppCompatActivity() {
     private lateinit var womanNameTextView: TextView
     private lateinit var manPhoneTextView: TextView
     private lateinit var womanPhoneTextView: TextView
+    private lateinit var dialogButton: AppCompatButton
+
+    private val TAG = "LIFECYCLE_TAG"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.contacts_detail_layout)
+
+        Log.d(TAG, "Detail Activity: onCreate")
 
         val contactMan: ContactVM = ContactVM("", "Dwight", "12345678910")
         val contactWoman: ContactVM = ContactVM("", "Claudette", "10987654321")
@@ -32,6 +41,7 @@ class ContactsDetailActivity : AppCompatActivity() {
         womanNameTextView = findViewById(R.id.name_woman_text_view)
         manPhoneTextView = findViewById(R.id.phone_man_text_view)
         womanPhoneTextView = findViewById(R.id.phone_woman_text_view)
+        dialogButton = findViewById(R.id.dialog_button)
 
         val contactStr: String? = intent.extras?.getString(Intent.EXTRA_TEXT)
         val contactReceived: ContactVM = Gson().fromJson(contactStr, ContactVM::class.java)
@@ -53,5 +63,50 @@ class ContactsDetailActivity : AppCompatActivity() {
             setResult(10002, intent)
             finish()
         })
+
+        dialogButton.setOnClickListener(View.OnClickListener {
+            // Use the Builder class for convenient dialog construction
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage("Message Dialog")
+                .setPositiveButton(R.string.confirm,
+                    DialogInterface.OnClickListener { dialog, id ->
+
+                    })
+                .setNegativeButton(R.string.cancel,
+                    DialogInterface.OnClickListener { dialog, id ->
+                    })
+            builder.create()
+            builder.show()
+        })
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Detail Activity: onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "Detail Activity: onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "Detail Activity: onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "Detail Activity: onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Detail Activity: onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "Detail Activity: onDestroy")
     }
 }
--- app/src/main/java/com/example/black_horse_onboarding/ContactsHomeActivity.kt
+++ app/src/main/java/com/example/black_horse_onboarding/ContactsHomeActivity.kt
@@ -3,6 +3,7 @@ package com.example.black_horse_onboarding
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
@@ -13,9 +14,12 @@ class ContactsHomeActivity : AppCompatActivity() {
     private lateinit var detailButton: AppCompatButton
     private val REQUEST_CODE: Int = 10001
 
+    private val TAG = "LIFECYCLE_TAG"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.contacts_home_layout)
+        Log.d(TAG, "Home Activity: onCreate")
 
         detailButton = findViewById(R.id.detail_button)
 
@@ -40,5 +44,35 @@ class ContactsHomeActivity : AppCompatActivity() {
             val contactSelected = Gson().fromJson(contactSelectedStr, ContactVM::class.java)
             Toast.makeText(this, "${contactSelected.name}: ${contactSelected.phone}", Toast.LENGTH_LONG).show()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Home Activity: onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "Home Activity: onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "Home Activity: onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "Home Activity: onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Home Activity: onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "Home Activity: onDestroy")
     }
 }
