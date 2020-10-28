7b69fb520e53eda34f5aac96995094c06b5b82fe
Leqi <michaelleqihust@gmail.com>
2020-10-28 13:06:01 +0800

    [leqi] feat: 完成Kotlin Coroutines 2作业。

--- app/src/main/java/com/example/black_horse_onboarding/CounterActivity.kt
+++ app/src/main/java/com/example/black_horse_onboarding/CounterActivity.kt
@@ -4,21 +4,27 @@ import android.os.*
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
-import kotlinx.coroutines.*
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import io.reactivex.rxjava3.kotlin.toObservable
 
 const val TIMER_UPDATE: Int = 1
 
 class CounterActivity : AppCompatActivity() {
     private lateinit var counterButton: AppCompatButton
-    private var counter: Int = 0
+    private lateinit var counters: MutableList<Int>
     private final val maxSecond = 10
+    private final val msgWhatTextChanged = 1
+    private final val msgWhatEnabledChanged = 2
+    private final val TAG: String = "Coroutines"
+    private lateinit var timerThread: Thread
 
     private val timerHandler = object : Handler(Looper.getMainLooper()) {
         override fun handleMessage(msg: Message) {
             super.handleMessage(msg)
-            updateButtonText()
-            if (counter == 10) {
-                counterButton.isEnabled = true
+            if (msg.what == msgWhatTextChanged) {
+                updateButtonText(msg.arg1)
+            } else if (msg.what == msgWhatEnabledChanged) {
+                setButtonEnabled(true)
             }
         }
     }
@@ -27,29 +33,57 @@ class CounterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_counter_layout)
 
-        counterButton = findViewById(R.id.counter_button)
-        counterButton.text = generateButtonText()
+        initThread()
+        initComponents()
+    }
 
-        // 抽象出一个方法
-        counterButton.setOnClickListener {
-            counter = 0
-            counterButton.isEnabled = false
+    private fun initThread() {
+        counters = mutableListOf()
+        for (index in 1..maxSecond) {
+            counters.add(index)
+        }
 
-            GlobalScope.launch(Dispatchers.Main) {
-                repeat(maxSecond) {
-                    delay(1000L)
-                    counter++
-                    timerHandler.sendEmptyMessage(TIMER_UPDATE)
-                }
+        timerThread = Thread {
+            kotlin.run {
+                counters.toObservable()
+                    .subscribeBy(
+                        onNext = {
+                            Log.d(TAG, "Loop Thread: ${Thread.currentThread().name}")
+                            SystemClock.sleep(1000L)
+                            val msg = Message()
+                            msg.what = msgWhatTextChanged
+                            msg.arg1 = it
+                            timerHandler.sendMessage(msg)
+                        },
+                        onError = { it.printStackTrace() },
+                        onComplete = {
+
+                        }
+                    )
             }
         }
     }
 
-    private fun generateButtonText(): String {
+    private fun initComponents() {
+        counterButton = findViewById(R.id.counter_button)
+        counterButton.text = generateButtonText(0)
+
+        counterButton.setOnClickListener {
+            Log.d(TAG, "Main Thread: ${Thread.currentThread().name}")
+            counterButton.isEnabled = false
+            timerThread.start()
+        }
+    }
+
+    private fun generateButtonText(counter: Int): String {
         return "${getString(R.string.counter_button_text)} $counter"
     }
 
-    private fun updateButtonText() {
-        counterButton.text = generateButtonText()
+    private fun updateButtonText(counter: Int) {
+        counterButton.text = generateButtonText(counter)
+    }
+
+    private fun setButtonEnabled(enabled: Boolean) {
+        counterButton.isEnabled = true
     }
 }
