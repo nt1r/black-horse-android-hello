c70229e82a1516df2c5ce1cd8974a49fa8104b07
Leqi <michaelleqihust@gmail.com>
2020-10-30 15:24:25 +0800

    [leqi] feat: 整理Coroutines作业。

--- app/src/main/java/com/example/black_horse_onboarding/CounterActivity.kt
+++ app/src/main/java/com/example/black_horse_onboarding/CounterActivity.kt
@@ -1,21 +1,29 @@
 package com.example.black_horse_onboarding
 
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
+import android.os.*
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import io.reactivex.rxjava3.kotlin.toFlowable
+import io.reactivex.rxjava3.kotlin.toObservable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.*
 import java.util.concurrent.TimeUnit
 
 const val TIMER_UPDATE: Int = 1
 
+@ExperimentalCoroutinesApi
 class CounterActivity : AppCompatActivity() {
+    private val SPACE_FLOW_OBSERVABLE_COROUTINES_DELAY: Int = 1
+    private val SPACE_FLOW_FLOWABLE_COROUTINES_DELAY: Int = 2
+    private val SPACE_FLOW_THREAD_SLEEP: Int = 3
+    private val TIME_FLOW_INTERVAL: Int = 4
+    private val method: Int = SPACE_FLOW_FLOWABLE_COROUTINES_DELAY
+
     private lateinit var counterButton: AppCompatButton
     private lateinit var counters: MutableList<Int>
     private var currentCount: Int = 0
@@ -46,47 +54,7 @@ class CounterActivity : AppCompatActivity() {
             counters.add(index)
         }
 
-        timerThread = initThread()
         initComponents()
-    }
-
-    private fun initThread(): Thread {
-        return Thread {
-            /*counters.toObservable()
-                .subscribeBy(
-                    onNext = {
-                        Log.d(TAG, "Loop Thread: ${Thread.currentThread().name}")
-                        SystemClock.sleep(1000L)
-                        val msg = Message()
-                        msg.what = msgWhatTextChanged
-                        msg.arg1 = it
-                        timerHandler.sendMessage(msg)
-                    },
-                    onError = { it.printStackTrace() },
-                    onComplete = {
-                        timerHandler.sendEmptyMessage(msgWhatEnabledChanged)
-                    }
-                )*/
-
-            currentCount = 0
-            subscription = Observable.interval(1000L, TimeUnit.MILLISECONDS)
-                .timeInterval()
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
-                    timerHandler.sendMessage(Message().apply {
-                        this.what = msgWhatTextChanged
-                        this.arg1 = ++currentCount
-                    })
-                    if (currentCount >= 10) {
-                        timerHandler.sendMessage(Message().apply {
-                            this.what = msgWhatEnabledChanged
-                        })
-                        subscription.dispose()
-                    }
-                }
-
-        }
     }
 
     private fun initComponents() {
@@ -96,8 +64,123 @@ class CounterActivity : AppCompatActivity() {
         counterButton.setOnClickListener {
             Log.d(TAG, "Main Thread: ${Thread.currentThread().name}")
             counterButton.isEnabled = false
-            timerThread = initThread()
-            timerThread.start()
+
+            when (method) {
+                SPACE_FLOW_OBSERVABLE_COROUTINES_DELAY -> {
+                    spaceFlowObservableCoroutinesDelay()
+                }
+                SPACE_FLOW_FLOWABLE_COROUTINES_DELAY -> {
+                    spaceFlowFlowableCoroutinesDelay()
+                }
+                SPACE_FLOW_THREAD_SLEEP -> {
+                    spaceFlowThreadSleep()
+                }
+                TIME_FLOW_INTERVAL -> {
+                    timeFlowInterval()
+                }
+            }
+        }
+    }
+
+    private fun spaceFlowFlowableCoroutinesDelay() {
+        GlobalScope.launch {
+            counters.toFlowable()
+                .observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribeBy(
+                    onNext = { counter ->
+                        runBlocking {
+                            Log.d(TAG, "Loop Thread: ${Thread.currentThread().name}")
+                            delay(1000L)
+                            updateButtonTextWithRunOnUiThread(counter)
+                        }
+                    },
+                    onError = { it.printStackTrace() },
+                    onComplete = {
+                        updateButtonEnabledWithRunOnUiThread()
+                    }
+                )
+        }
+    }
+
+    private fun timeFlowInterval() {
+        currentCount = 0
+        subscription = Observable.interval(1000L, TimeUnit.MILLISECONDS)
+            .timeInterval()
+            .observeOn(Schedulers.io())
+            .subscribeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                updateButtonTextWithRunOnUiThread(++currentCount)
+                if (currentCount >= 10) {
+                    updateButtonEnabledWithRunOnUiThread()
+                    subscription.dispose()
+                }
+            }
+    }
+
+    private fun spaceFlowThreadSleep() {
+        timerThread = Thread {
+            counters.toObservable()
+                .observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribeBy(
+                    onNext = { counter ->
+                        Log.d(TAG, "Loop Thread: ${Thread.currentThread().name}")
+                        SystemClock.sleep(1000L)
+                        updateButtonTextWithRunOnUiThread(counter)
+                    },
+                    onError = { it.printStackTrace() },
+                    onComplete = {
+                        updateButtonEnabledWithRunOnUiThread()
+                    }
+                )
+        }
+        timerThread.start()
+    }
+
+    private fun spaceFlowObservableCoroutinesDelay() {
+        GlobalScope.launch(Dispatchers.IO) {
+            counters.toObservable()
+                .observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribeBy(
+                    onNext = { count ->
+                        runBlocking {
+                            Log.d(TAG, "Loop Thread: ${Thread.currentThread().name}")
+                            delay(1000L)
+
+                            updateButtonTextWithHandler(count)
+                        }
+                    },
+                    onError = { it.printStackTrace() },
+                    onComplete = {
+                        updateButtonEnabledWithHandler()
+                    }
+                )
+        }
+    }
+
+    private fun updateButtonEnabledWithHandler() {
+        timerHandler.sendEmptyMessage(msgWhatEnabledChanged)
+    }
+
+    private fun updateButtonTextWithHandler(count: Int) {
+        val msg = Message().apply {
+            this.what = msgWhatTextChanged
+            this.arg1 = count
+        }
+        timerHandler.sendMessage(msg)
+    }
+
+    private fun updateButtonTextWithRunOnUiThread(count: Int) {
+        runOnUiThread {
+            updateButtonText(count)
+        }
+    }
+
+    private fun updateButtonEnabledWithRunOnUiThread() {
+        runOnUiThread {
+            setButtonEnabled(true)
         }
     }
 
@@ -110,6 +193,6 @@ class CounterActivity : AppCompatActivity() {
     }
 
     private fun setButtonEnabled(enabled: Boolean) {
-        counterButton.isEnabled = true
+        counterButton.isEnabled = enabled
     }
 }
